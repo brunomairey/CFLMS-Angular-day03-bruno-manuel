a97a68ef00e29dde472bb9432032096bef6103b6
Gloria Prantner <gloriaprantner@gmail.com>
2020-06-24 11:05:16 +0200

    added angular project

---
+++ Bruno-Gloria/petShop/src/app/product.ts
@@ -0,0 +1,66 @@
+export const products = [
+	{
+		name:"Cereal for Cats",
+		price: 28,
+		animal:"Cat",
+		description:"A nice breakfast for your cat. It will love it totally!",
+		img:"",
+		available: true,
+	},{
+		name:"Carrots",
+		price: 57,
+		animal:"Prairie Dog",
+		description:"Your Prairie Dog will love this carrots. They are just made for them. Feeding other animals with this carrots is not allowed. If you are searching for carrots for Rabbits or Llamas buy them here.",
+		img:"",
+		available: true
+	},{
+		name:"Rabbit salad",
+		price: 45,
+		animal:"Rabbit",
+		description:"This salad is born to be eaten by your rabbit. After eating this salad your rabbit will be cured from all ",
+		img:"",
+		available:
+	},{
+		name:"",
+		price: ,
+		animal:"",
+		description:"",
+		img:"",
+		available:
+	},{
+		name:"",
+		price: ,
+		animal:"",
+		description:"",
+		img:"",
+		available:
+	},{
+		name:"",
+		price: ,
+		animal:"",
+		description:"",
+		img:"",
+		available:
+	},{
+		name:"",
+		price: ,
+		animal:"",
+		description:"",
+		img:"",
+		available:
+	},{
+		name:"",
+		price: ,
+		animal:"",
+		description:"",
+		img:"",
+		available:
+	},{
+		name:"",
+		price: ,
+		animal:"",
+		description:"",
+		img:"",
+		available:
+	}
+];
